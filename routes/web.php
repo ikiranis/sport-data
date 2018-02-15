1d2144f3816cfb12ea6caff253f4ecd89296c577
rocean <rocean74@gmail.com>
2018-02-15 09:10:07 +0200

    Routes testing

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'The Site';
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('paok', function () {
-    return 'paok';
+Route::get('/admin', function () {
+    return view('admin.index');
 });
+
