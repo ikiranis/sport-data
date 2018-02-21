be6100848e1a20c51f650c671bc2f83896a9a3f7
rocean <rocean74@gmail.com>
2018-02-21 03:32:58 +0200

    Display images on sports cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,20 @@
     <div class="container">
         <div class="row justify-content-center">
 
+            @php $counter=5; @endphp
             @foreach($sports as $sport)
+
                 <div class="col-md-3">
                     <div class="card card-default">
                         <div class="card-header">{{$sport->name}}</div>
 
                         <div class="card-body">
-
+                            <img src="http://lorempixel.com/400/200/sports/{{$counter}}"
+                                 class="img-fluid">
                         </div>
                     </div>
                 </div>
+                @php $counter++; @endphp
             @endforeach
 
         </div>
