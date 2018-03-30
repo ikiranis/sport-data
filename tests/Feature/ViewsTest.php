b0283e4a4403086409f4b246db6662b866015e9a
rocean <rocean74@gmail.com>
2018-03-31 01:25:08 +0300

    Test edit views

--- tests/Feature/ViewsTest.php
+++ tests/Feature/ViewsTest.php
@@ -59,6 +59,31 @@ class ViewsTest extends TestCase
     }
 
     /**
+     * Test Athletes edit view response
+     */
+    public function testAthletesEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $athlete = Athlete::first(); // First athlete in database
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/athletes/{$athlete->id}/edit");
+
+        $response->assertStatus(200);  // Test if view loading
+
+        $response->assertViewHas('athlete'); // Test if "athletes" data exist
+
+        $responseAthlete = (object) $response->original['athlete']->first(); // First athlete in view response
+
+        if($athlete->fname==$responseAthlete->fname) {  // Test if fname field is ok
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
      * Test Stadia Index view response
      */
     public function testStadiaIndexViewResponse()
@@ -97,6 +122,31 @@ class ViewsTest extends TestCase
         $response->assertStatus(200);  // Test if view loading
 
         $response->assertSee('content'); // Test if it has content
+    }
+
+    /**
+     * Test Stadia edit view response
+     */
+    public function testStadiaEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $stadium = Stadium::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/stadium/{$stadium->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('stadium'); //
+
+        $responseStadium = (object) $response->original['stadium']->first();
+
+        if($stadium->name==$responseStadium->name) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
     }
 
     /**
@@ -141,6 +191,31 @@ class ViewsTest extends TestCase
     }
 
     /**
+     * Test Teams edit view response
+     */
+    public function testTeamsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $team = Team::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/teams/{$team->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('team'); //
+
+        $responseTeam = (object) $response->original['team']->first();
+
+        if($team->name==$responseTeam->name) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
      * Test Sports Index view response
      */
     public function testSportsIndexViewResponse()
@@ -179,6 +254,31 @@ class ViewsTest extends TestCase
         $response->assertStatus(200);  // Test if view loading
 
         $response->assertSee('content'); // Test if it has content
+    }
+
+    /**
+     * Test Sports edit view response
+     */
+    public function testSportsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $sport = Sport::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/sports/{$sport->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('sport'); //
+
+        $responseSport = (object) $response->original['sport']->first();
+
+        if($sport->name==$responseSport->name) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
     }
 
     /**
@@ -223,6 +323,31 @@ class ViewsTest extends TestCase
     }
 
     /**
+     * Test Seasons edit view response
+     */
+    public function testSeasonsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $season = Season::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/seasons/{$season->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('season'); //
+
+        $responseSeason = (object) $response->original['season']->first();
+
+        if($season->name==$responseSeason->name) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
      * Test Championships Index view response
      */
     public function testChampionshipsIndexViewResponse()
@@ -264,6 +389,31 @@ class ViewsTest extends TestCase
     }
 
     /**
+     * Test Championships edit view response
+     */
+    public function testChampionshipsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $championship = Championship::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/championships/{$championship->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('championship');
+
+        $responseChampionship = (object) $response->original['championship']->first();
+
+        if($championship->name==$responseChampionship->name) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
      * Test Matchdays Index view response
      */
     public function testMatchdaysIndexViewResponse()
@@ -281,7 +431,7 @@ class ViewsTest extends TestCase
 
         $responseMatchdays = (object) $response->original['matchdays']->first();
 
-        if($matchdays->name==$responseMatchdays->name) {
+        if($matchdays->matchday === $responseMatchdays->matchday) {
             $this->assertTrue(true);
         } else {
             $this->assertTrue(false);
@@ -302,6 +452,31 @@ class ViewsTest extends TestCase
         $response->assertStatus(200);  // Test if view loading
 
         $response->assertSee('content'); // Test if it has content
+    }
+
+    /**
+     * Test Matchdays edit view response
+     */
+    public function testMatchdaysEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $matchday = Matchday::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/matchdays/{$matchday->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('matchday'); //
+
+        $responseMatchday = (object) $response->original['matchday']->first();
+
+        if($matchday->matchday==$responseMatchday->matchday) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
     }
 
     /**
@@ -346,6 +521,31 @@ class ViewsTest extends TestCase
     }
 
     /**
+     * Test Posts edit view response
+     */
+    public function testPostsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $post = Post::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/posts/{$post->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('post'); //
+
+        $responsePost = (object) $response->original['post']->first();
+
+        if($post->title==$responsePost->title) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
+    }
+
+    /**
      * Test Comments Index view response
      */
     public function testCommentsIndexViewResponse()
@@ -369,6 +569,31 @@ class ViewsTest extends TestCase
             $this->assertTrue(false);
         }
 
+    }
+
+    /**
+     * Test Comments edit view response
+     */
+    public function testCommentsEditViewResponse()
+    {
+        $user = User::whereRoleId(1)->first();
+
+        $comment = Comment::first();
+
+        $response = $this->actingAs($user, 'web')
+            ->get("/admin/comments/{$comment->id}/edit");
+
+        $response->assertStatus(200);
+
+        $response->assertViewHas('comment'); //
+
+        $responseComment = (object) $response->original['comment']->first();
+
+        if($comment->email==$responseComment->email) {
+            $this->assertTrue(true);
+        } else {
+            $this->assertTrue(false);
+        }
     }
 
     /**
