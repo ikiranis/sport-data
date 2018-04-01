9c3d666464449f1ad2189df173d87399b5094261
rocean <rocean74@gmail.com>
2018-04-01 21:42:44 +0300

    Clean code

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-03-31 17:43:15.
+    * Generated on 2018-04-01 14:37:24.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
