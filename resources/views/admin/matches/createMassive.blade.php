f4f7793871267ab909408d60d77bad4275483266
rocean <rocean74@gmail.com>
2018-04-19 01:35:47 +0300

    Improve code

--- resources/views/admin/matches/createMassive.blade.php
+++ resources/views/admin/matches/createMassive.blade.php
@@ -21,58 +21,59 @@
                 @for($counter=0; $counter<(count($teams)/2); $counter++)
                     <tr id="matchesContainer{{$counter}}">
 
-                            <td>
+                        <td>
 
-                                <div class="input-group mb-3 no-gutters">
-                                    <label for="first_team_id" class="sr-only">{{__('messages.team')}}</label>
-                                    <select v-model="firstTeamSelected" v-on:change="checkValidTeam"
-                                            class="form-control col-5 px-2"
-                                            id="first_team_id" name="first_team_id">
-                                        <option value="0"></option>
-                                        <option v-for="team in teams" :value="team.id">{% team.name %}</option>
-                                    </select>
+                            <div class="input-group mb-3 no-gutters">
+                                <label for="first_team_id" class="sr-only">{{__('messages.team')}}</label>
+                                <select v-model="firstTeamSelected" v-on:change="checkValidTeam"
+                                        class="form-control col-5 px-2"
+                                        id="first_team_id" name="first_team_id">
+                                    <option value="0"></option>
+                                    <option v-for="team in teams" :value="team.id">{% team.name %}</option>
+                                </select>
 
-                                    <label for="second_team_id" class="sr-only">{{__('messages.team')}}</label>
-                                    <select v-model="secondTeamSelected" v-on:change="checkValidTeam"
-                                            class="form-control col-5 px-2"
-                                            id="second_team_id" name="second_team_id">
-                                        <option value="0"></option>
-                                        <option v-for="team in teams" :value="team.id">{% team.name %}</option>
-                                    </select>
-                                </div>
+                                <label for="second_team_id" class="sr-only">{{__('messages.team')}}</label>
+                                <select v-model="secondTeamSelected" v-on:change="checkValidTeam"
+                                        class="form-control col-5 px-2"
+                                        id="second_team_id" name="second_team_id">
+                                    <option value="0"></option>
+                                    <option v-for="team in teams" :value="team.id">{% team.name %}</option>
+                                </select>
+                            </div>
 
-                            </td>
+                        </td>
 
-                            <td>
-                                <div class="input-group mb-3 no-gutters">
-                                    <label for="stadium_id" class="sr-only">{{__('messages.stadium')}}</label>
-                                    <select v-model="stadiumSelected" class="form-control col-10 px-2" id="stadium_id" name="stadium_id">
-                                        @foreach($stadia as $stadium)
-                                            <option value="{{$stadium->id}}">
-                                                {{$stadium->name}}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </td>
+                        <td>
+                            <div class="input-group mb-3 no-gutters">
+                                <label for="stadium_id" class="sr-only">{{__('messages.stadium')}}</label>
+                                <select v-model="stadiumSelected" class="form-control col-10 px-2" id="stadium_id"
+                                        name="stadium_id">
+                                    @foreach($stadia as $stadium)
+                                        <option value="{{$stadium->id}}">
+                                            {{$stadium->name}}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </td>
 
-                            <td>
-                                <div class="input-group mb-3 no-gutters">
-                                    <label class="sr-only" for="match_date">{{__('messages.date')}}</label>
-                                    <input type="date" class="form-control col-10 px-2" id="match_date"
-                                           name="match_date">
-                                </div>
+                        <td>
+                            <div class="input-group mb-3 no-gutters">
+                                <label class="sr-only" for="match_date">{{__('messages.date')}}</label>
+                                <input type="date" class="form-control col-10 px-2" id="match_date"
+                                       name="match_date">
+                            </div>
 
-                            </td>
+                        </td>
 
 
-                            <td>
-                                <button v-on:click="postData" type="submit" class="btn"
-                                        v-bind:class="isSaved ? 'btn-success' : 'btn-outline-success'"
-                                        v-bind:disabled="isSaved">
-                                    Save
-                                </button>
-                            </td>
+                        <td>
+                            <button v-on:click="postData" type="submit" class="btn"
+                                    v-bind:class="isSaved ? 'btn-success' : 'btn-outline-success'"
+                                    v-bind:disabled="isSaved">
+                                Save
+                            </button>
+                        </td>
                     </tr>
                 @endfor
 
@@ -104,58 +105,62 @@
             }
         }
 
-
-        @for($counter=0; $counter<(count($teams)/2); $counter++)
-        new Vue({
-            delimiters: ['{%', '%}'],
-            el: "#matchesContainer{{$counter}}",
-            data: {
-                firstTeamSelected: '{!! $data->first_team_id ?? 0 !!}',
-                secondTeamSelected: '{!! $data->second_team_id ?? 0 !!}',
-                teams: @json($teams),
-                sport_id: '{!! $data->sport_id ?? 0 !!}',
-                championship_id: '{!! $data->championship_id ?? 0 !!}',
-                season_id: '{!! $data->season_id ?? 0 !!}',
-                matchday_id: '{!! $data->matchday_id ?? 0 !!}',
-                stadiumSelected: '',
-                isSaved: false
-            },
-            methods: {
-                checkValidTeam(e) { // Check if the other team is the same
-                    if (this.firstTeamSelected === this.secondTeamSelected) {
-
-                        let changedSelectElement = e.target.id;
-
-                        if (changedSelectElement === 'first_team_id') {
-                            this.firstTeamSelected = this.teams[getIndexInArray(this.teams, this.firstTeamSelected)].id;
-                        } else {
-                            this.secondTeamSelected = this.teams[getIndexInArray(this.teams, this.secondTeamSelected)].id;
-                        }
-
-                    }
-                },
-                postData() {
-
-                    let myData = {
-                        sport_id: this.sport_id,
-                        championship_id: this.championship_id,
-                        season_id: this.season_id,
-                        matchday_id: this.matchday_id,
-                        first_team_id: this.firstTeamSelected,
-                        second_team_id: this.secondTeamSelected,
-                        stadium_id: this.stadiumSelected
-                    };
-
-                    axios.post('/api/match', myData)
-                        .then(response => {
-                            this.isSaved = true;
-                        })
-                        .catch(e => console.log(e));
-                },
-            }
-        });
-        @endfor
-
     </script>
+
+    @for($counter=0; $counter<(count($teams)/2); $counter++)
+        <script>
+
+            new Vue({
+                delimiters: ['{%', '%}'],
+                el: "#matchesContainer{{$counter}}",
+                data: {
+                    firstTeamSelected: '{!! $data->first_team_id ?? 0 !!}',
+                    secondTeamSelected: '{!! $data->second_team_id ?? 0 !!}',
+                    teams: @json($teams),
+                    sport_id: '{!! $data->sport_id ?? 0 !!}',
+                    championship_id: '{!! $data->championship_id ?? 0 !!}',
+                    season_id: '{!! $data->season_id ?? 0 !!}',
+                    matchday_id: '{!! $data->matchday_id ?? 0 !!}',
+                    stadiumSelected: '',
+                    isSaved: false
+                },
+                methods: {
+                    checkValidTeam(e) { // Check if the other team is the same
+                        if (this.firstTeamSelected === this.secondTeamSelected) {
+
+                            let changedSelectElement = e.target.id;
+
+                            if (changedSelectElement === 'first_team_id') {
+                                this.firstTeamSelected = this.teams[getIndexInArray(this.teams, this.firstTeamSelected)].id;
+                            } else {
+                                this.secondTeamSelected = this.teams[getIndexInArray(this.teams, this.secondTeamSelected)].id;
+                            }
+
+                        }
+                    },
+                    postData() {
+
+                        let myData = {
+                            sport_id: this.sport_id,
+                            championship_id: this.championship_id,
+                            season_id: this.season_id,
+                            matchday_id: this.matchday_id,
+                            first_team_id: this.firstTeamSelected,
+                            second_team_id: this.secondTeamSelected,
+                            stadium_id: this.stadiumSelected
+                        };
+
+                        axios.post('/api/match', myData)
+                            .then(response => {
+                                this.isSaved = true;
+                            })
+                            .catch(e => console.log(e));
+                    },
+                }
+            });
+
+
+        </script>
+    @endfor
 
 @endsection
