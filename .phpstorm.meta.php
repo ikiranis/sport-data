41788a2c1b261ec19739465c57eddc85d51f3d37
rocean <rocean74@gmail.com>
2018-04-24 19:32:22 +0300

    Update laravel

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-22 14:32:22.
+    * Generated on 2018-04-24 16:19:10.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
