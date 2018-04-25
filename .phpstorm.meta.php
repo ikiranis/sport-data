efbd89b0e55b25622f0505466e9c2eeb6850b434
rocean <rocean74@gmail.com>
2018-04-26 02:53:04 +0300

    Test code

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-04-24 16:19:10.
+    * Generated on 2018-04-25 23:52:18.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
