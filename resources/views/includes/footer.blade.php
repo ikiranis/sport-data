418754a1416fbec71e2d81468fbcc2ce61d6ee75
rocean <rocean74@gmail.com>
2018-05-12 00:16:12 +0300

    Fix logo path

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,7 +5,7 @@
                 Επικοινωνία: <span class="font-weight-bold">rocean74 (at) gmail.com</span>
             </div>
             <div class="col text-right">
-                Created by <a href="http://www.apps4net.eu"><img src="images/site/apps4net_logo.jpg"></a>
+                Created by <a href="http://www.apps4net.eu"><img src="{{url('images/site/apps4net_logo.jpg')}}"></a>
             </div>
         </div>
     </div>
