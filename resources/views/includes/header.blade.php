a2c2dbba68d5e9247b7dd21c31f8760f4c946152
rocean <rocean74@gmail.com>
2018-05-12 00:27:21 +0300

    Header styling

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
-<div class="container-fluid">
-    <div class="row bg-dark text-white px-5" id="siteTitle">
-        <a href="{{ url('/') }}"><span class="mdi mdi-home-outline"></span>{{ config('app.name', 'Laravel') }}</a>
+<div class="container-fluid bg-dark text-white px-5">
+    <div class="text-center" id="siteTitle">
+        <a href="{{ url('/') }}"><span class="mdi mdi-24px mdi-home-outline"></span></a>
     </div>
 </div>
