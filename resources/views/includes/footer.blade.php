e1bcf9ba53453b66b81b443a9d5c45779ad96c43
rocean <rocean74@gmail.com>
2018-05-12 00:11:12 +0300

    Footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,14 @@
+<div class="container mb-3">
+    <div class="card">
+        <div class="card-body row">
+            <div class="col text-left">
+                Επικοινωνία: <a href="mailto:rocean74@gmail.com">rocean74@gmail.com</a>
+            </div>
+            <div class="col text-right">
+                Created by <a href="http://www.apps4net.eu"><img src="images/site/apps4net_logo.jpg"></a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="{{ mix('js/app.js') }}"></script>
