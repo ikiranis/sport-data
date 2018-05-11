5cab596eea87708541508bb07182090f1b1888de
rocean <rocean74@gmail.com>
2018-05-11 23:51:36 +0300

    Home icon

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,5 @@
-<div class="row bg-dark text-white px-5" id="siteTitle">
-    <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+<div class="container-fluid">
+    <div class="row bg-dark text-white px-5" id="siteTitle">
+        <a href="{{ url('/') }}"><span class="mdi mdi-home-outline"></span>{{ config('app.name', 'Laravel') }}</a>
+    </div>
 </div>
