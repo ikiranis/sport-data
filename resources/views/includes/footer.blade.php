29bd0c017b7be6da048806018c4477019b5d2f61
rocean <rocean74@gmail.com>
2018-05-15 00:08:00 +0300

    RSS Icon

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,12 +1,23 @@
 <div class="container mb-3">
     <div class="card">
-        <div class="card-body row">
-            <div class="col text-left">
-                Επικοινωνία: <span class="font-weight-bold">rocean74 (at) gmail.com</span>
+        <div class="card-body row align-middle">
+
+            <div class="col text-left my-auto">
+                    <div>Επικοινωνία: <span class="font-weight-bold">rocean74 (at) gmail.com</span></div>
             </div>
-            <div class="col text-right">
-                Created by <a href="http://www.apps4net.eu"><img src="{{url('images/site/apps4net_logo.jpg')}}"></a>
+
+            <div class="col text-center my-auto">
+                <a href="{{ url('/feed') }}"><img src="{{url('images/site/RSS.png')}}" width="30"
+                                                       title="RSS News Feed"></a>
             </div>
+
+            <div class="col text-right my-auto">
+                <span>
+                    Created by <a href="http://www.apps4net.eu"><img
+                                src="{{url('images/site/apps4net_logo.jpg')}}"></a>
+                </span>
+            </div>
+
         </div>
     </div>
 </div>
