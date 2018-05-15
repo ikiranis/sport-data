a38b45f28564994efb137c7238a395d3a682afec
rocean <rocean74@gmail.com>
2018-05-16 02:23:17 +0300

    Fix meta tags

--- resources/views/includes/post-single.blade.php
+++ resources/views/includes/post-single.blade.php
@@ -3,34 +3,40 @@
 @endsection
 
 @section('shareMetaTags')
-    <meta name="description" content="{{ $post->description }}" />
+    <meta name="description" content="{{ $post->description }}"/>
 
     <!-- Schema.org markup for Google+ -->
     <meta itemprop="name" content="{{ $post->title }}">
     <meta itemprop="description" content="{{ $post->description }}">
-    <meta itemprop="image" content="{{ $post->photo ? $post->photo->full_path_name : ''}}">
+    <meta itemprop="image" content="{{ $post->photo ? secure_url($post->photo->full_path_name) : ''}}">
 
     <!-- Twitter Card data -->
-    <meta name="twitter:card" content="{{ $post->photo ? $post->photo->full_path_name : ''}}">
-    <meta name="twitter:site" content="">
+    <meta name="twitter:card" content="{{ $post->photo ? secure_url($post->photo->full_path_name) : ''}}">
+    {{--<meta name="twitter:site" content="">--}}
     <meta name="twitter:title" content="{{ $post->title }}">
     <meta name="twitter:description" content="{{ $post->description }}">
-    <meta name="twitter:creator" content="">
+    {{--<meta name="twitter:creator" content="">--}}
     <!-- Twitter summary card with large image must be at least 280x150px -->
-    <meta name="twitter:image:src" content="{{ $post->photo ? $post->photo->full_path_name : ''}}">
+    <meta name="twitter:image:src" content="{{ $post->photo ? secure_url($post->photo->full_path_name) : ''}}">
 
     <!-- Open Graph data -->
-    {{--<meta property="og:title" content="{{ $post->title }}" />--}}
-    {{--<meta property="og:type" content="article" />--}}
-    {{--<meta property="og:url" content="{{ url('/post/' . $post->slug }}" />--}}
-    {{--<meta property="og:image" content="{{ $post->photo ? $post->photo->full_path_name : ''}}" />--}}
-    {{--<meta property="og:description" content="{{ $post->description }}" />--}}
-    {{--<meta property="og:site_name" content="West Macedonia Sports" />--}}
-    {{--<meta property="article:published_time" content="{{ $post->created_at }}" />--}}
-    {{--<meta property="article:modified_time" content="{{ $post->updated_at }}" />--}}
-    {{--<meta property="article:section" content="Article Section" />--}}
-    {{--<meta property="article:tag" content="{{ $post->teams() }}" />--}}
-    {{--<meta property="fb:admins" content="Facebook numberic ID" />--}}
+    <meta property="og:title" content="{{ $post->title }}"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="{{ secure_url('post/' . $post->slug) }}"/>
+    <meta property="og:image" content="{{ $post->photo ? secure_url($post->photo->full_path_name) : ''}}"/>
+    <meta property="og:description" content="{{ $post->description }}"/>
+    <meta property="og:site_name" content="West Macedonia Sports"/>
+    <meta property="article:published_time" content="{{ $post->created_at }}"/>
+    <meta property="article:modified_time" content="{{ $post->updated_at }}"/>
+    {{--<meta property="article:section" content="Article Section"/>--}}
+    @php
+        $tags = '';
+        foreach($post->teams as $team) {
+             $tags = $tags . $team->name . ' ';
+        }
+    @endphp
+    <meta property="article:tag" content="{{ $tags }}"/>
+    {{--<meta property="fb:admins" content="Facebook numberic ID"/>--}}
 
 @endsection
 
