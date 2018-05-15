7264373e945176bb4bb0de50861ad95737c66722
rocean <rocean74@gmail.com>
2018-05-16 01:24:58 +0300

    Change fonts

--- resources/views/includes/teams-results.blade.php
+++ resources/views/includes/teams-results.blade.php
@@ -16,7 +16,7 @@
             <tr>
                 <td>{{ $match->match_date ? $match->match_date->format('d/m/Y') : 'TBA' }}</td>
                 <td class="col-8">{{ $match->teams }}</td>
-                <td class="col-4 text-right font-weight-bold">{{$match->first_team_score}} - {{$match->second_team_score}}</td>
+                <td class="col-4 text-right font-weight-bold">{{$match->first_team_score}}-{{$match->second_team_score}}</td>
             </tr>
         @endforeach
 
