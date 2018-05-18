26d1c43bf31a8cac56d281889848d5650018d270
rocean <rocean74@gmail.com>
2018-05-18 22:31:55 +0300

    Display stadium name

--- resources/views/includes/teams-results.blade.php
+++ resources/views/includes/teams-results.blade.php
@@ -6,6 +6,7 @@
         <thead>
         <tr>
             <th scope="col" class="text-center">Ημερομηνία</th>
+            <th scope="col" class="text-center">Γήπεδο</th>
             <th scope="col" class="text-center">Αγώνας</th>
             <th scope="col" class="text-right">Σκορ</th>
         </tr>
@@ -15,8 +16,9 @@
         @foreach($matchdayMatches as $match)
             <tr>
                 <td>{{ $match->match_date ? $match->match_date->format('d/m/Y') : 'TBA' }}</td>
+                <td class="col-2">{{ $match->stadium->name }}</td>
                 <td class="col-8">{{ $match->teams }}</td>
-                <td class="col-4 text-right font-weight-bold">{{$match->first_team_score}}-{{$match->second_team_score}}</td>
+                <td class="col-2 text-right font-weight-bold">{{$match->first_team_score}}-{{$match->second_team_score}}</td>
             </tr>
         @endforeach
 
