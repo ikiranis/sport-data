0b5bc821a52f6972c83d8155b40f098aaf9595b1
rocean <rocean74@gmail.com>
2018-05-19 19:03:52 +0300

    tinyMCE media plugin

--- resources/views/includes/editor.blade.php
+++ resources/views/includes/editor.blade.php
@@ -12,7 +12,14 @@
 
 
 <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=1azfr0zhlf5i17ungsfoyb11cc6cu4abmnizwww1wtamncu8"></script>
-<script>tinymce.init({ selector:'#body' });</script>
+<script>
+    tinymce.init({
+        selector:'#body',
+        height: 500,
+        theme: 'modern',
+        plugins: "media mediaembed",
+    });
+</script>
 
 
 {{--<script src="https://cdn.ckeditor.com/ckeditor5/10.0.0/decoupled-document/ckeditor.js"></script>--}}
