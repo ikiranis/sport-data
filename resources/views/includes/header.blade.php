06a2a421096bd90e9e74fc510d6233686a7dec39
rocean <rocean74@gmail.com>
2018-05-20 23:58:17 +0300

    Responsive navbar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,19 +2,26 @@
 
     <a class="navbar-brand" href="{{ route('home') }}"><span class="mdi mdi-home-outline"></span>WMSports</a>
 
-    <ul class="navbar-nav my-auto ml-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('about') }}">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('contact') }}">Επικοινωνία</a>
-        </li>
-        <li class="nav-item my-auto">
-            <a href="{{ secure_url('/feed') }}">
-                <img src="/images/site/RSS.png" width="25" title="RSS News Feed">
-            </a>
-        </li>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-    </ul>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav my-auto ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('about') }}">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('contact') }}">Επικοινωνία</a>
+            </li>
+            <li class="nav-item my-auto text-center">
+                <a href="{{ secure_url('/feed') }}">
+                    <img src="/images/site/RSS.png" width="25" title="RSS News Feed">
+                </a>
+            </li>
+
+        </ul>
+    </div>
 
 </nav>
