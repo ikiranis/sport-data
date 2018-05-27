22e0872ff0ee693282c4100276fcf39275ad83fa
rocean <rocean74@gmail.com>
2018-05-28 01:11:44 +0300

    npm to production

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-19 19:24:46.
+    * Generated on 2018-05-27 22:09:47.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
