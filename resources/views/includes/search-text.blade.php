f26b4ef27a801bc371133f7d478ac27005b8d2ed
rocean <rocean74@gmail.com>
2018-05-29 02:02:19 +0300

    Search input responsive

--- resources/views/includes/search-text.blade.php
+++ resources/views/includes/search-text.blade.php
@@ -1,7 +1,7 @@
 <div class="container my-3">
     <form method="GET" action="{{ route('search') }}">
 
-        <div class="input-group mb-3 no-gutters col-6 ml-auto mr-auto">
+        <div class="input-group mb-3 no-gutters col-lg-6 col-12 ml-auto mr-auto">
             <label class="sr-only" for="search">Αναζήτηση</label>
             <input type="text" max="255" class="form-control col-10 px-2" id="search" name="search">
             <button class="btn btn-success col-2">Αναζήτηση</button>
