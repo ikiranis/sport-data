f1911b4b75ab013be880407a7a9f0a531797cda5
rocean <rocean74@gmail.com>
2018-05-29 02:03:53 +0300

    Search input responsive

--- resources/views/includes/search-text.blade.php
+++ resources/views/includes/search-text.blade.php
@@ -3,8 +3,8 @@
 
         <div class="input-group mb-3 no-gutters col-lg-6 col-12 ml-auto mr-auto">
             <label class="sr-only" for="search">Αναζήτηση</label>
-            <input type="text" max="255" class="form-control col-10 px-2" id="search" name="search">
-            <button class="btn btn-success col-2">Αναζήτηση</button>
+            <input type="text" max="255" class="form-control col-8 px-2" id="search" name="search">
+            <button class="btn btn-success col-4">Αναζήτηση</button>
         </div>
 
         @csrf
