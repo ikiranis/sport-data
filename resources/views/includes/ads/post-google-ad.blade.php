8c571bb0449af00e6a78e8f61cd1199e11411165
rocean <rocean74@gmail.com>
2018-06-25 03:42:32 +0300

    Center all ads

--- resources/views/includes/ads/post-google-ad.blade.php
+++ resources/views/includes/ads/post-google-ad.blade.php
@@ -1,9 +1,13 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- wmsposrts_post_ad -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-4611864067867074"
-     data-ad-slot="5691228462"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<div class="row p-2">
+    <div class="ml-auto mr-auto">
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <!-- wmsposrts_post_ad -->
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:728px;height:90px"
+             data-ad-client="ca-pub-4611864067867074"
+             data-ad-slot="5691228462"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+    </div>
+</div>
